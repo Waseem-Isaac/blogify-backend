498f72b3db54ef6fb1e7fed6c8128d0f949c1a83
WaseemIsaacIbrahim <waseem.isaac@businessboomers.net>
2020-08-16 18:25:30 +0200

    return user date with single post api

--- models/post.js
+++ models/post.js
@@ -5,12 +5,16 @@ const postSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    likes : {
+        type: [String],
+    },
     comments: [{ content: String }],
     user: { 
         type : mongoose.Schema.Types.ObjectId , 
         ref : 'User',
         required: true
-    }
+    },
+    
 }, {versionKey : false });
 
 module.exports = mongoose.model('Post' , postSchema);
